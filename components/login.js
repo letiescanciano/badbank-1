ab45a112e66c0787189ba399ef33758dbd0f3643
Carmen Constantinescu <87343796+ciconstantinescu@users.noreply.github.com>
2022-01-04 12:15:01 -0500

    Add files via upload

---
+++ components/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {HashRouter} from 'react-router-dom';
+import { Route } from 'react-router-dom';
+
+function Login(){
+    return (
+      <h1>Login Form</h1>
+    )  
+  };
+
+  export default Login;
+  
